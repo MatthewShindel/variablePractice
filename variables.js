56e622f6feed027726e83ec3681ba9984415b4bd
MatthewShindel <matthew@shindel.com>
2023-09-15 15:31:22 -0400

    I added some console.log statements, as well as reassigned a number variable

--- variables.js
+++ variables.js
@@ -2,8 +2,15 @@ var firstName = "Matthew"
 var lastName = "Shindel"
 var middleInital = "A"
 
-var age = "25"
-var bankAccount = "0"
+var age = 25
+var bankAccount = 0
 
 var isStudent = true
 var isSingle = true
+
+console.log("My name is " + firstName+ " " + middleInital + " " + lastName + ".")
+console.log("I am" +" "+ age + " "+ "years old.")
+
+bankAccount = 3000
+
+console.log("I found some money, now I have " + bankAccount + " dollars.")
